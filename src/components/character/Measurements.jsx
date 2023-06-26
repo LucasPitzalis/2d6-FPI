1df3113a80e14d76fea049fae4c01591c8472c35
LucasPitzalis <lpitzalis@yahoo.fr>
2023-06-26 10:02:17 +0200

    icons sizing back to static

--- src/components/character/Measurements.jsx
+++ src/components/character/Measurements.jsx
@@ -3,9 +3,9 @@ import LockedInput from "./LockedInput";
 export default function Measurements() {
     return (
         <div className="flex space-x-0.5">
-            <LockedInput label="âge" name="age" styles="w-1/3" />
-            <LockedInput label="taille" name="height" styles="w-1/3" />
-            <LockedInput label="poids" name="weight" styles="w-1/3" />
+            <LockedInput label="âge" name="age" styles="w-1/3" htmlType="number" />
+            <LockedInput label="taille" name="height" styles="w-1/3" htmlType="number" />
+            <LockedInput label="poids" name="weight" styles="w-1/3" htmlType="number" />
         </div>
     );
 }
--- src/icons/CancelIcon.jsx
+++ src/icons/CancelIcon.jsx
@@ -10,8 +10,8 @@ CancelIcon.defaultProps = {
 
 export default function CancelIcon({ size }) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-${size} h-${size}`}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3 h-3">
+            <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
         </svg>
     );
 }
