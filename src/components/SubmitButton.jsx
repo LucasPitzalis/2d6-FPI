270a064e8cc82714c4d1b26fcb23a42b86ead706
LucasPitzalis <lpitzalis@yahoo.fr>
2023-06-27 14:28:28 +0200

    styling on SubmitButton when disabled

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 
-export default function SubmitButton({ text }) {
+export default function SubmitButton({ text, isDisabled }) {
     return (
         <button 
             type="submit"
             className="rounded-full bg-teal-800 font-bold mt-2 text-white py-1 px-3 
                 outline hover:outline-2 hover:outline-amber-500
-                "
+                disabled:outline-none disabled:bg-gray-500 disabled:italic disabled:font-normal"
+            disabled={isDisabled}
         >
             { text }
         </button>
@@ -15,4 +16,9 @@ export default function SubmitButton({ text }) {
 
 SubmitButton.propTypes = {
     text: PropTypes.string.isRequired,
+    isDisabled: PropTypes.string,
 };
+
+SubmitButton.defaultProps = {
+    isDisabled: false,
+}
