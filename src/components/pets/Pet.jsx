31462b4cce183681ae58d9b42499dd24d50a729d
LucasPitzalis <lpitzalis@yahoo.fr>
2023-08-22 11:00:55 +0200

    input rework really done

--- src/components/pets/Pet.jsx
+++ src/components/pets/Pet.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import LockedInput from "../fields/LockedInput";
+import InputField from "../fields/InputField";
 import StaticField from "../fields/StaticField";
 import PropTypes from 'prop-types';
 import DeletableItem from "../DeletableItem";
@@ -24,24 +24,24 @@ export default function Pet({ pet, index, deleteMode }) {
                 <div className="flex flex-col justify-between space-y-0.5 w-full md:w-1/2">
                     <div className="flex space-x-0.5">
                         <StaticField label="n°" name={`pets.${index}`} value={index + 1} styles={'w-16'} />
-                        <LockedInput label="nom" name={`pets.${index}.name`} styles={'w-full'} />
+                        <InputField label="nom" name={`pets.${index}.name`} styles={'w-full'} />
                     </div>
                     <div className="flex space-x-0.5">
                         <StaticField label="niv." name={`pets.${index}.level`} value={currentPetLevel(index).level} styles={'w-40'} />
-                        <LockedInput label="xp acquis" name={`pets.${index}.experience`} center styles={'w-full'} htmlType="number" />
+                        <InputField label="xp acquis" name={`pets.${index}.experience`} center styles={'w-full'} htmlType="number" />
                         <StaticField label="xp prochain" name={`pets.${index}.xpToReach`} value={currentPetLevel(index).nextLevelXp} styles={'w-full'} />
                     </div>
                 </div>
                 <div className="flex flex-col justify-between space-y-0.5 w-full md:w-1/2">
                     <div className="flex space-x-0.5">
-                        <LockedInput label="nb. de pt. pv/pe" name={`pets.${index}.hpEpPts`} htmlType="number" center styles={'w-1/2'} />
-                        <LockedInput label="pt. compétences" name={`pets.${index}.skillPts`} htmlType="number" center styles={'w-1/2'} />
+                        <InputField label="nb. de pt. pv/pe" name={`pets.${index}.hpEpPts`} htmlType="number" center styles={'w-1/2'} />
+                        <InputField label="pt. compétences" name={`pets.${index}.skillPts`} htmlType="number" center styles={'w-1/2'} />
                     </div>
                     <div className="flex space-x-0.5">
-                        <LockedInput label="PV" name={`pets.${index}.healthPts`} htmlType="number" center styles={'w-1/3'} />
-                        <LockedInput name={`pets.${index}.maxHp`} htmlType="number" center styles={'w-1/6'} />
-                        <LockedInput label="PE" name={`pets.${index}.energyPts`} htmlType="number" center styles={'w-1/3'} />
-                        <LockedInput name={`pets.${index}.maxEp`} htmlType="number" center styles={'w-1/6'} />
+                        <InputField label="PV" name={`pets.${index}.healthPts`} htmlType="number" center styles={'w-1/3'} />
+                        <InputField name={`pets.${index}.maxHp`} htmlType="number" center styles={'w-1/6'} />
+                        <InputField label="PE" name={`pets.${index}.energyPts`} htmlType="number" center styles={'w-1/3'} />
+                        <InputField name={`pets.${index}.maxEp`} htmlType="number" center styles={'w-1/6'} />
                     </div>
                 </div>
             </div>
@@ -49,20 +49,20 @@ export default function Pet({ pet, index, deleteMode }) {
                 <Avatar />
                 <div className="flex flex-col justify-between space-y-0.5 w-full md:w-1/2">
                     <div className="flex space-x-0.5">
-                        <LockedInput label={{text: 'attaque', icon: <AttackIcon />}} name={`pets.${index}.atkDesc`} styles={'w-full'} />
-                        <LockedInput name={`pets.${index}.atkValue`} htmlType="number" center styles={'w-12'} />
+                        <InputField label={{text: 'attaque', icon: <AttackIcon />}} name={`pets.${index}.atkDesc`} styles={'w-full'} />
+                        <InputField name={`pets.${index}.atkValue`} htmlType="number" center styles={'w-12'} />
                     </div>
                     <div className="flex space-x-0.5">
-                        <LockedInput label={{text: 'défense', icon: <DefenseIcon />}} name={`pets.${index}.defDesc`} styles={'w-full'} />
-                        <LockedInput name={`pets.${index}.defValue`} htmlType="number" center styles={'w-12'} />
+                        <InputField label={{text: 'défense', icon: <DefenseIcon />}} name={`pets.${index}.defDesc`} styles={'w-full'} />
+                        <InputField name={`pets.${index}.defValue`} htmlType="number" center styles={'w-12'} />
                     </div>
                     <div className="flex space-x-0.5">
-                        <LockedInput label={{text: 'volonté', icon: <WillIcon />}} name={`pets.${index}.wilDesc`} styles={'w-full'} />
-                        <LockedInput name={`pets.${index}.wilValue`} htmlType="number" center styles={'w-12'} />
+                        <InputField label={{text: 'volonté', icon: <WillIcon />}} name={`pets.${index}.wilDesc`} styles={'w-full'} />
+                        <InputField name={`pets.${index}.wilValue`} htmlType="number" center styles={'w-12'} />
                     </div>
                     <div className="flex space-x-0.5">
-                        <LockedInput label={{text: 'spécial', icon: <SpecialIcon />}} name={`pets.${index}.speDesc`} styles={'w-full'} />
-                        <LockedInput name={`pets.${index}.speValue`} htmlType="number" center styles={'w-12'} />
+                        <InputField label={{text: 'spécial', icon: <SpecialIcon />}} name={`pets.${index}.speDesc`} styles={'w-full'} />
+                        <InputField name={`pets.${index}.speValue`} htmlType="number" center styles={'w-12'} />
                     </div>
                 </div>
             </div>
