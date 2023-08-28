c7fce0408ea9b6bee7c02b997b6316e70c26c726
LucasPitzalis <lpitzalis@yahoo.fr>
2023-08-28 12:05:48 +0200

    added editing of pet level rolls + proper layout for them

--- src/components/modals/EditPetRolls.jsx
+++ src/components/modals/EditPetRolls.jsx
@@ -11,7 +11,7 @@ export default function EditPetRolls() {
     const pets = useSelector((state) => state.pets);
     const [rolls, setRolls] = useState(pets[petIndex].levelRolls);
 
-    const pendingRolls = (pet) => getLevel(pet.experience, true).level - rolls.length;
+    const pendingRolls = (pet) => getLevel(pet.experience, true).level - pet.levelRolls.length;
 
     useEffect(() => setRolls(pets[petIndex].levelRolls), [petIndex])
 
@@ -23,8 +23,17 @@ export default function EditPetRolls() {
         const newRolls = [...rolls];
         for(let i = 1; i <= pendingRolls(pets[petIndex]); i++) {
             const currentLevel = currentPetLevel(petIndex).level + i - pendingRolls(pets[petIndex]);
-            newRolls.push({hpEp: rollDice(petLevelsTable[currentLevel - 1].hpEpDice), skillPts: rollDice(petLevelsTable[currentLevel - 1].skillDice)})
+            newRolls.push({hpEp: rollDice(petLevelsTable[currentLevel - 1].hpEpDice), skill: rollDice(petLevelsTable[currentLevel - 1].skillDice)})
         }
+        setRolls(newRolls);
+    }
+
+    function updateRoll(e) {
+        const [index, property] = e.target.name.split('.');
+        const diceAmount = petLevelsTable[index][`${property}Dice`];
+        if(e.target.value > diceAmount * 6 || e.target.value < diceAmount) return;
+        const newRolls = [...rolls];
+        newRolls[index] = {...rolls[index], [property]: e.target.value};
         setRolls(newRolls);
     }
 
@@ -41,13 +50,33 @@ export default function EditPetRolls() {
                     )}
                 </select>
             </div>
-            <div className="flex flex-wrap flex-col justify-center items-center space-x-2">
-                <SimpleButton text={"Lancer les dés"} handler={rollPendingLevels} isDisabled={!pendingRolls(pets[petIndex])} />
-                {rolls.length !== 0 && rolls.map((roll, index) => 
-                    <span key={`level${index + 1}`}>
-                        {`Niv. ${index + 1} : Pts de PV/PE : ${roll.hpEp} - Pts de compétences : ${roll.skillPts}`}
-                    </span>
-                )}
+            <div className="flex flex-wrap flex-col justify-center items-center">
+                <SimpleButton text={"Lancer les dés"} handler={rollPendingLevels} isDisabled={currentPetLevel(petIndex).level === rolls.length} />
+                <div className="flex flex-col justify-center items-center mt-1 [&>*:not(:last-child)]:pb-1 [&>*:not(:last-child)]:border-b [&>*:not(:last-child)]:border-gray-400">
+                    {rolls.length !== 0 && rolls.map((roll, index) => 
+                        <div key={`level${index + 1}`} className="flex space-x-2 pt-1">
+                            <div className="min-w-[40px] flex justify-center items-center font-bold flex-nowrap">{`Niv. ${index + 1} :`}</div>
+                            <div className="flex flex-wrap justify-center items-center gap-y-1 gap-x-2">
+                                <div className="flex flex-nowrap justify-center items-center">
+                                    <span>Pts PV/PE</span>
+                                    <input 
+                                        className="ml-1 w-12 text-center border border-black rounded disabled:inactive" 
+                                        type="number" name={`${index}.hpEp`} step="1" 
+                                        value={roll.hpEp} onChange={updateRoll}
+                                    />
+                                </div>
+                                <div className="flex flex-nowrap justify-center items-center">
+                                    <span>Pts comp.</span>
+                                    <input 
+                                        className="ml-1 w-12 text-center border border-black rounded disabled:inactive" 
+                                        type="number" name={`${index}.skill`} step="1" 
+                                        value={roll.skill} onChange={updateRoll}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </form>
     );
