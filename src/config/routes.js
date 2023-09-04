a3f6be9f695196b0ece8a57daf3c51602b945e08
LucasPitzalis <lpitzalis@yahoo.fr>
2023-09-04 12:12:11 +0200

    uncommented pets route after merge with main branch

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,7 +3,7 @@ import Character from "../components/character/Character";
 import Contacts from "../components/contacts/Contacts";
 import CoverLetter from "../components/coverLetter/CoverLetter";
 import Items from "../components/items/Items";
-// import Pets from "../components/pets/Pets";
+import Pets from "../components/pets/Pets";
 import Skills from "../components/skills/Skills";
 
 const routes = [
@@ -11,7 +11,7 @@ const routes = [
     {name: 'skills', nameFr: 'Compétences', path: '/competences', element: Skills, sheet: true},
     {name: 'contacts', nameFr: 'Contacts', path: '/contacts', element: Contacts, sheet: true},
     {name: 'items', nameFr: 'Objets', path: '/objets', element: Items, sheet: true},
-    // {name: 'pets', nameFr: 'Familiers', path: '/familier', element: Pets, sheet: true},
+    {name: 'pets', nameFr: 'Familiers', path: '/familier', element: Pets, sheet: true},
     {name: 'coverLetter', nameFr: 'Lettre de motivation', path: '/lettre-de-motivation', element: CoverLetter, sheet: true},
     {name: 'about', nameFr: 'À propos', path: '/a-propos', element: About, sheet: false}
 ]
